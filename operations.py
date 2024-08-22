b96d87d946b4398caf2f942176b2f23d30421ae1
NeemaMwende <gracepete12@gmail.com>
2024-08-22 18:44:28 +0300

    operations in python

---
+++ operations.py
@@ -0,0 +1,16 @@
+sub = 10 - 5
+print(sub)
+
+div = 9 // 3
+print(div)
+
+a = 5
+b = 2
+print( a > b )
+
+#comparison operators - decision making and loops
+# ==, !=, >, <, >=, <=
+
+#logical operators - and, or, not
+print((a>2) or (b>2))
+
