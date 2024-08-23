ccd3a05b550285633fbce21253d9ba257876c0fb
NeemaMwende <gracepete12@gmail.com>
2024-08-23 12:57:13 +0300

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,21 @@
+#example
+# capital_city = {"Nepal":"Kathmandu", "Italy":"Rome"}
+# print(capital_city)
+
+# capital_city["Japan"]="Tokyo"
+# print(capital_city)
+
+# del capital_city["Nepal"]
+# print(capital_city)
+
+# capital_city["Italy"] = "Nairobi"
+# print(capital_city)
+
+squares = {1:1, 3:9, 5:25, 7:49, 9:81, 10:100}
+# print(squares)
+# print(1 in squares)
+# print(2 not in squares)
+# print(49 in squares)
+
+for i in squares:
+    print(squares[i])
