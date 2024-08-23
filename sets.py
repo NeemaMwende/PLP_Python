f446c4eef962dda28dbd52c5c925458145c89472
NeemaMwende <gracepete12@gmail.com>
2024-08-23 13:30:20 +0300

    set updates

--- sets.py
+++ sets.py
@@ -9,16 +9,36 @@ print('Student Id:', student_id)
 # student_id.add(120)
 # print( student_id)
 
-companies = {'Lacoste', "Ralph Lauren"}
-tech_companies = ['apple', 'google', 'apple']
-companies.update(tech_companies)
-print(companies)
-companies.discard('apple')
-print(companies)
+# companies = {'Lacoste', "Ralph Lauren"}
+# tech_companies = ['apple', 'google', 'apple']
+# companies.update(tech_companies)
+# print(companies)
+# companies.discard('apple')
+# print(companies)
 
-fruits = {"Apple", "Peach", "Mango"}
-for fruit in fruits:
-    print(fruit)
+# fruits = {"Apple", "Peach", "Mango"}
+# for fruit in fruits:
+#     print(fruit)
 
-print(len(companies))
-print(len(tech_companies))
+# print(len(companies))
+# print(len(tech_companies))
+
+#union of sets
+A = {1, 2, 3, 4}
+B = {5, 3, 6, 7, 8}
+print(A | B)
+print(A.union(B))
+
+print(A & B)
+print(A.intersection(B))
+
+print(A - B)
+print(A.difference(B))
+
+print(A ^ B) #not common in all
+print(A.symmetric_difference(B))
+
+if A==B:
+    print("They are equal")
+else:
+    print("Not equal")
