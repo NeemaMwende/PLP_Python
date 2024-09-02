c1ededfa08759d869b9a7c1a47b3c450ed55778b
NeemaMwende <gracepete12@gmail.com>
2024-09-03 00:10:17 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,22 @@
+# def thing():
+#     print("hello")
+#     print("fun")
+# thing()
+# print("zip")
+# thing()
+
+# big = max('Hello world')
+# print(big)
+
+# tiny = min("Hello world")
+# print(tiny)
+
+def greet(language):
+    if language == "espaniol":
+        print("Hola")
+    elif language == "french":
+        print("Bonjour")
+    else:
+        print("Hello")
+
+greet("french")
