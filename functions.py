9e153dc287ddfe012fd6db29848db9d0fd4ca270
NeemaMwende <gracepete12@gmail.com>
2024-09-03 00:34:58 +0300

    updates

--- functions.py
+++ functions.py
@@ -11,12 +11,25 @@
 # tiny = min("Hello world")
 # print(tiny)
 
-def greet(language):
-    if language == "espaniol":
-        print("Hola")
-    elif language == "french":
-        print("Bonjour")
-    else:
-        print("Hello")
+# def greet(language):
+#     if language == "espaniol":
+#         print("Hola")
+#     elif language == "french":
+#         print("Bonjour")
+#     else:
+#         print("Hello")
 
-greet("french")
+# greet("french")
+
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+print(factorial(5))  # Output: 120
