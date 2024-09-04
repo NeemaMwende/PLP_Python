ca189915fd39daa39fa23d2a3580709774ba546c
NeemaMwende <gracepete12@gmail.com>
2024-09-04 18:30:59 +0300

    local, global, built in scopes variables

--- functions.py
+++ functions.py
@@ -21,15 +21,50 @@
 
 # greet("french")
 
-def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers."
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        result = 1
-        for i in range(2, n + 1):
-            result *= i
-        return result
+# def factorial(n):
+#     if n < 0:
+#         return "Factorial is not defined for negative numbers."
+#     elif n == 0 or n == 1:
+#         return 1
+#     else:
+#         result = 1
+#         for i in range(2, n + 1):
+#             result *= i
+#         return result
 
-print(factorial(5))  # Output: 120
+# print(factorial(5))  # Output: 120
+
+# def add_nums(a, b):
+#     answer = a + b
+#     return answer
+
+# total = add_nums(2, 3)
+# print(total)
+
+# def add_nums(*nums):
+#     sum = 0
+#     for num in nums:
+#         sum += num
+#     return sum
+# print("Total: ", add_nums(2, 5, 6, 10, 100, 1000))
+
+# def add_ages(**ages):
+#     sum = 0
+#     for k, v in ages.items():
+#         sum += v
+#     return sum
+# print("Total: ", add_ages(angel= 23, Lexin=24, Victor=25))
+
+global_var = 13
+
+def add_nums(a, b):
+    total = a + b
+    print("Global variable in outer function: ", global_var)
+    def double_it():
+        #local variable
+        double = total * 2
+        print("Global variable in inner function: ", global_var)
+    double_it()
+    return total
+
+add_nums(13, 5)
