4455c8c0cf8a58f630e3685bbeb1cfe3fa4655dd
NeemaMwende <gracepete12@gmail.com>
2024-09-06 14:59:30 +0300

    introduction to OOP

---
+++ OOP.py
@@ -0,0 +1,11 @@
+# Building blocks of OOP
+# Classes - a blueprint of an object
+# Objects - an instance of a class
+# Methods - methods represent behaviors
+# Attributes - information to be stored in a class about an object
+
+# Inheritance: child classes inherit data and behaviors from the parent class
+# Encapsulation: containing information in an object, exposing only selected information
+# Abstraction: only exposing high-level public methods for accessing an object
+# Polymorphism: many methods can do the same task
+
--- loops.py
+++ loops.py
@@ -26,12 +26,37 @@
 #     print(count)
 #     count += 1
 
-colors = ["blue", "green", "white", "yellow", "brown", "cream"]
-color_i_want = "white"
+# colors = ["blue", "green", "white", "yellow", "brown", "cream"]
+# color_i_want = "white"
 
-for color in colors:
+# for color in colors:
     
-    if color == color_i_want:
-        print("They have the color i want")
-        break
-    print(color)
+#     if color == color_i_want:
+#         print("They have the color i want")
+#         break
+#     print(color)
+
+# largest_so_far = -1
+# print("Before", largest_so_far)
+# for the_num in [9, 41, 12, 3, 74, 15]:
+#     if the_num > largest_so_far:
+#         largest_so_far = the_num
+#     print(largest_so_far, the_num)
+# print("After", largest_so_far)
+
+# n = 5
+# while n > 0:
+#     print(n)
+# print("All done")
+
+smallest_so_far = -1
+for the_num in [9, 41, 12, 3, 74, 15] :
+   if the_num < smallest_so_far :
+      smallest_so_far = the_num
+print(smallest_so_far)
+
+n = 0
+while n > 0 :
+    print('Lather')
+    print('Rinse')
+print('Dry off!')
