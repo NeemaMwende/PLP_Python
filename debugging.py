1961a11ecc15e55ea595246618990c4f41dd5709
NeemaMwende <gracepete12@gmail.com>
2024-09-07 01:33:57 +0300

    debugging updates

--- debugging.py
+++ debugging.py
@@ -7,12 +7,24 @@
 # print(f"The final result is {result}")
 
 #using pdb, python's interactive source code debugger : n for next, c for continue, q for quit
-import pdb;
+# import pdb;
+# def add_numbers(a, b):
+#     pdb.set_trace()
+#     result = a + b
+#     return result
+# sum_result = add_numbers(5, 3)
+# print(f"sum: {sum_result}")
 
-def add_numbers(a, b):
-    pdb.set_trace()
-    result = a + b
-    return result
+#breakpoint debugger : By default, breakpoint() will import pdb and call pdb.set_trace()
+# PYTHONBREAKPOINT=0
+# def multiply(a, b):
+#     breakpoint()
+#     result = a * b
+#     return result
+# product = multiply(5, 3)
+# print(f"Product: {product}")
 
-sum_result = add_numbers(5, 3)
-print(f"sum: {sum_result}")
+#python -m pdb my_script.py (another way is to use this in the terminal instead of breakpoint and pdb's)
+filename = __file__
+import pdb; pdb.set_trace()
+print(f"path = {filename}")
